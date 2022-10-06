468f48cfff7c72e663a2911fdcd85f581b4b0271
JoranEngelund <joranengelund@hotmail.com>
2022-10-06 16:18:16 +0200

    added responsive on project-card and function to check screen width

--- js/script.js
+++ js/script.js
@@ -2,8 +2,19 @@ const carouselContainer = document.querySelector(".content-container");
 const nextBtn = document.querySelector(".next");
 const prevBtn = document.querySelector(".prev");
 
-const width = 360;
+let width = 360;
 const gap = 16;
+
+function checkScreenSize() {
+  const screenWidth = window.innerWidth;
+  if (screenWidth <= 396) {
+    width = 264;
+  } else if (screenWidth >= 397) {
+    width = 360;
+  }
+}
+
+window.addEventListener("resize", checkScreenSize);
 
 function nextSlide() {
   carouselContainer.scrollBy(width + gap, 0);
